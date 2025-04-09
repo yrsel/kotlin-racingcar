279ea65a1634b6f14df79656cda9da2dc2bc285c
yrsel <xxknu88@gmail.com>
2025-04-09 20:01:47 +0900

    feat: Move car position

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,3 +1,17 @@
 package domain
 
-class Car(val name: String)
+class Car(val name: String, position: Int) {
+    var position = position
+        private set
+
+    fun move(number: Int) {
+        if (number in MOVABLE_LOWER_BOUND..MOVABLE_UPPER_BOUND) {
+            position++
+        }
+    }
+
+    companion object {
+        private const val MOVABLE_LOWER_BOUND = 4
+        private const val MOVABLE_UPPER_BOUND = 9
+    }
+}
--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,16 +1,33 @@
 import domain.Car
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     @Test
     fun `자동차에 이름을 부여한다`() {
         // given
-        val car = Car("chestnut")
+        val car = Car("chestnut", 0)
         val expected = "chestnut"
 
         // when
         val actual = car.name
+
+        // then
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `자동차는 4 이상 9 이하 숫자가 들어온다면 이동거리가 1 증가한다`(number: Int) {
+        // given
+        val car = Car("chestnut", 0)
+        val expected = 1
+
+        // when
+        car.move(number)
+        val actual = car.position
 
         // then
         Assertions.assertThat(actual).isEqualTo(expected)
