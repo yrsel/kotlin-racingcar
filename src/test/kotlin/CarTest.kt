9e5e7f1b8998533db4a379871e019f07a959cc44
yrsel <xxknu88@gmail.com>
2025-04-09 20:55:31 +0900

    refactor: Add car fixture

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,14 +1,22 @@
 import domain.Car
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
+    private lateinit var chestnutCar: Car
+
+    @BeforeEach
+    fun setUp() {
+        chestnutCar = Car("chestnut", 0)
+    }
+
     @Test
     fun `자동차에 이름을 부여한다`() {
         // given
-        val car = Car("chestnut", 0)
+        val car = chestnutCar
         val expected = "chestnut"
 
         // when
@@ -22,7 +30,7 @@ class CarTest {
     @ValueSource(ints = [4, 5, 6, 7, 8, 9])
     fun `자동차는 4 이상 9 이하 숫자가 들어온다면 이동거리가 1 증가한다`(number: Int) {
         // given
-        val car = Car("chestnut", 0)
+        val car = chestnutCar
         val expected = 1
 
         // when
@@ -37,7 +45,7 @@ class CarTest {
     @ValueSource(ints = [0, 1, 2, 3])
     fun `자동차는 4 미만의 숫자가 들어올 경우 이동하지 않는다`(number: Int) {
         // given
-        val car = Car("chestnut", 0)
+        val car = chestnutCar
         val expected = 0
 
         // when
