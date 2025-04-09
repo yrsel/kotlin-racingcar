ea1a673a85259ee88f548dc43523887ca6045622
yrsel <xxknu88@gmail.com>
2025-04-09 20:10:30 +0900

    feat: Generate RandomNumber For MoveCondition

---
+++ src/main/kotlin/domain/RandomNumberGenerator.kt
@@ -0,0 +1,14 @@
+package domain
+
+import kotlin.random.Random
+
+class RandomNumberGenerator : NumberGenerator {
+    override fun generateNumber(): Int {
+        return Random.nextInt(NUMBER_LOWER_BOUND, NUMBER_UPPER_BOUND)
+    }
+
+    companion object {
+        private const val NUMBER_LOWER_BOUND = 0
+        private const val NUMBER_UPPER_BOUND = 9
+    }
+}
