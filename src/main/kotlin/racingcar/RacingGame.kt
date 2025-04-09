36c515e0a43f3d801f432a783de9a0376c2557b5
yrsel <xxknu88@gmail.com>
2025-04-09 21:26:43 +0900

    feat: Read try count input

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -7,11 +7,16 @@ import racingcar.view.ConsoleView
 class RacingGame {
     fun play() {
         val cars = cars()
+        val tryCount = tryCount()
     }
 
     private fun cars(): Cars {
         val cars: List<Car> = ConsoleView.readCarNames()
         return Cars(cars)
+    }
+
+    private fun tryCount(): Int {
+        return ConsoleView.readTryCount()
     }
 }
 
--- src/main/kotlin/racingcar/view/ConsoleView.kt
+++ src/main/kotlin/racingcar/view/ConsoleView.kt
@@ -5,9 +5,15 @@ import racingcar.domain.Car
 object ConsoleView {
     private const val CAR_NAME_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     private const val CAR_NAME_DELIMITER = ","
+    private const val TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
 
     fun readCarNames(): List<Car> {
         println(CAR_NAME_GUIDE_MESSAGE)
         return readln().split(CAR_NAME_DELIMITER).map { name -> Car(name.trim()) }
+    }
+
+    tailrec fun readTryCount(): Int {
+        println(TRY_COUNT_MESSAGE)
+        return readln().toIntOrNull() ?: readTryCount()
     }
 }
